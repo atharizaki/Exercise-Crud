4d1fecec646f4ee405f95ba05040270079453f6a
atharizaki <atharizaki4@gmail.com>
2022-06-17 12:38:29 +0700

    Add project files.

---
+++ Exercise Crud/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_Crud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals("admin") && textBox2.Text.Equals("123"))
+            {
+                new Form2().Show();
+                this.Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("Periksa kembali Username dan Id anda");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Exercise Crud/Form2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_Crud
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-3O1VMEB;Initial Catalog=StoreProcedure_DB;User ID=sa;Password=atharizaki123");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int empid = int.Parse(textBox1.Text);
+            string nama = textBox2.Text, asal = comboBox1.Text, Jenis_Kelamin = "";
+            DateTime TanggalLahir = DateTime.Parse(dateTimePicker1.Text);
+            if(radioButton1.Checked==true) { Jenis_Kelamin = "Laki-Laki"; } else { Jenis_Kelamin = "Perempuan"; }
+            double umur = double.Parse(textBox3.Text);
+            con.Open();
+            SqlCommand c = new SqlCommand("exec InsertEMP_SP '" + empid + "','" + nama + "','" + asal + "','" + umur + "','" + Jenis_Kelamin + "','" + TanggalLahir + "' ", con);
+            c.ExecuteNonQuery();
+            MessageBox.Show("Data Berhasil Di input");
+            GetEmpList();
+        }
+
+        void GetEmpList()
+        {
+            SqlCommand c = new SqlCommand("exec ListEmp_SP", con);
+            SqlDataAdapter sd = new SqlDataAdapter(c);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            GetEmpList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int empid = int.Parse(textBox1.Text);
+            string nama = textBox2.Text, asal = comboBox1.Text, Jenis_Kelamin = "";
+            DateTime TanggalLahir = DateTime.Parse(dateTimePicker1.Text);
+            if (radioButton1.Checked == true) { Jenis_Kelamin = "Laki-Laki"; } else { Jenis_Kelamin = "Perempuan"; }
+            double umur = double.Parse(textBox3.Text);
+            con.Open();
+            SqlCommand c = new SqlCommand("exec UpdateEmp_SP '" + empid + "','" + nama + "','" + asal + "','" + umur + "','" + Jenis_Kelamin + "','" + TanggalLahir + "' ", con);
+            c.ExecuteNonQuery();
+            MessageBox.Show("Data Berhasil Di Update");
+            GetEmpList();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Apakah Yakin ingin Dihapus?", "Delete Document", MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                int empid = int.Parse(textBox1.Text);
+                con.Open();
+                SqlCommand c = new SqlCommand("exec DeleteEmp_SP '" + empid + "' ", con);
+                c.ExecuteNonQuery();
+                MessageBox.Show("Data Berhasil Di Hapus");
+                GetEmpList();
+            }
+            
+        }
+    }
+}
